20045d412dc8b5eddd6022936212c056e9b8ba2b
sayednadim <smnadimuddin@gmail.com>
2020-06-22 15:14:45 +0900

    Update main

--- HarmonySearch.py
+++ HarmonySearch.py
@@ -75,7 +75,7 @@ class objective_function:
     def __init__(self,
                  input_X,
                  input_Y,
-                 iteration=1000,
+                 iteration=5000,
                  weight_decimal=0,
                  sample_size=-1,
                  hmcr_proba=0.7,
@@ -108,72 +108,12 @@ class objective_function:
 
     def fitness(self, weight, input_X, input_Y):
         e = 0.0
-        # 新產生的權重正規化
-        weight = [float(i) / sum(weight) for i in weight]
-        # e += sum(np.multiply(input_X,weight)) - input_Y
-        for x, y in zip(input_X, input_Y):
-            # print('Compare',sum(np.multiply(x,weight)),y)
-            mean = 0.5 * np.abs(np.mean(y) - np.mean(x))
-            std = 0.5 * np.abs(np.std(y) - np.std(x))
-            e += mean + std
+        # weight = [float(i) / sum(weight) for i in weight]
+        mean = 0.5 * np.abs(np.mean(input_Y) - np.mean(input_X))
+        std = 0.5 * np.abs(np.std(input_Y) - np.std(input_X))
+        e += mean + std
+        # for x, y in zip(input_X, input_Y):
+
         # e /= np.array(input_X).shape[0]
         # print(e)
         return e
-
-
-# if __name__ == '__main__':
-#     '''
-#     Load your data.(input_X & input_Y)
-#     input_X & input_Y both are matrix like this:
-#     input_X = [[ 1, 0, 0, 1 ],
-#      [ 1 , 0, 0, 1 ],
-#      [ 0 , 1, 1, 0 ],
-#      [ 0 , 1, 1, 0 ],
-#      [ 0 , 1, 1, 0 ],
-#      [ 1 , 0, 0, 1 ]]
-#
-#     input_Y = [[0],
-#      [0],
-#      [1],
-#      [1],
-#      [1],
-#      [0]]
-#     '''
-#     input_X = [[1, 0, 0, 1]]
-#
-#     input_Y = [[1, 1, 0, 1]]
-#     '''
-#     init interface obj
-#     objective_function = objective_function("here your input_X","here your input_Y")
-#     here you can customer your parameter or not ( run as define ).
-#     For example:
-#     up_down_limit = [[0,1],[0,1],[0,1],[0,1]]
-#     '''
-#     up_down_limit = [[0, 1], [0, 1], [0, 1], [0, 1]]
-#
-#     '''
-#     You can customize your in __init__ func or here.
-#     '''
-#     objective_function = objective_function(input_X, input_Y, sample_size=6, weight_decimal=2,
-#                                             up_down_limit=up_down_limit)
-#
-#     '''
-#     pass your interface obj into a harmony core obj.
-#     hs = HarmonyCore("here your interface obj")
-#     '''
-#     hs = HarmonyCore(objective_function)
-#
-#     '''
-#     run harmony
-#     hs.run()
-#     '''
-#     hmm_vector, hmm_err_list, err_idx = hs.run()
-#
-#     '''
-#     then you can get (hmm_vector,hmm_err_list,err_idx) after hs return.
-#     '''
-#
-#     print('HMM_Vector:', hmm_vector)
-#     print('Best HMM_Vector:', hmm_vector[err_idx])
-#     print('hmm_err_list:', hmm_err_list)
-#     print('err_index:', err_idx, 'hmm_err:', hmm_err_list[err_idx])
--- main.py
+++ main.py
@@ -4,50 +4,53 @@ from utils import *
 patch_size = 3
 x_im = imageio.imread('tiger.jpg')
 x_hint = imageio.imread('tiger_color.jpg')
-x_im = np.resize(x_im, (256, 256, 3))
-x_hint = np.resize(x_hint, (256, 256, 3))
-
-patch_size = 3
+x_im = np.resize(x_im, (32, 32, 3))
+x_hint = np.resize(x_hint, (32, 32, 3))
 yiq_gray, yiq_color = color_space_conversion(x_im, x_hint)
 M = x_im.shape[0] // patch_size
 N = x_im.shape[1] // patch_size
-grid = np.zeros((M, N))
 yiq_gray_luminance = yiq_gray[:, :, 0]
 yiq_color_luminance = yiq_color[:, :, 0]
 yiq_gray_patches = create_patches(yiq_gray_luminance, patch_size, patch_size)
 yiq_color_patches = create_patches(yiq_color_luminance, patch_size, patch_size)
 
-gray_array =[]
+gray_array = []
 color_array = []
 
+# for i in range(M):
+#     for j in range(N):
+#         grays = img2vector(yiq_gray_patches[i][j])
+#         colors = img2vector(yiq_color_patches[i][j])
+#         gray_array.append(grays)
+#         color_array.append(colors)
+#
+# print(len(gray_array))
+
+best_values = []
 for i in range(M):
     for j in range(N):
-        grays = img2vector(yiq_gray_patches[i][j])
-        colors = img2vector(yiq_color_patches[i][j])
-        gray_array.append(grays)
-        color_array.append(colors)
+        of = objective_function(yiq_gray_patches[i][j], yiq_color_patches[i][j], sample_size=6, weight_decimal=2, )
+        hs = HarmonyCore(of)
+        '''
+        run harmony
+        hs.run()
+        '''
+        hmm_vector, hmm_err_list, err_idx = hs.run()
+        best_values.append(hmm_vector[err_idx])
 
+        '''
+        then you can get (hmm_vector,hmm_err_list,err_idx) after hs return.
+        '''
+        # print('HMM_Vector:', hmm_vector)
+        print('Best HMM_Vector:', hmm_vector[err_idx])
+        # print('hmm_err_list:', hmm_err_list)
+        # print('err_index:', err_idx, 'hmm_err:', hmm_err_list[err_idx])
+        print('{}{}th patch done'.format(i, j))
 
-
-population = []
-for i in range(len(gray_array)):
-    of = objective_function(gray_array[i], color_array[i], sample_size=6, weight_decimal=2, )
-    hs = HarmonyCore(of)
-    '''
-    run harmony
-    hs.run()
-    '''
-    hmm_vector, hmm_err_list, err_idx = hs.run()
-    population.append(hmm_vector[err_idx])
-
-    '''
-    then you can get (hmm_vector,hmm_err_list,err_idx) after hs return.
-    '''
-
-    # print('HMM_Vector:', hmm_vector)
-    print('Best HMM_Vector:', hmm_vector[err_idx])
-    print('hmm_err_list:', hmm_err_list)
-    print('err_index:', err_idx, 'hmm_err:', hmm_err_list[err_idx])
-    print('{}th patch done'.format(i))
-
-
+best_values = np.reshape(best_values, (M, N, patch_size, patch_size))
+print(best_values)
+final_image = reconstruct_image(best_values)
+print(final_image.shape)
+import matplotlib.pyplot as plt
+plt.imshow(final_image, aspect='auto')
+plt.show()
